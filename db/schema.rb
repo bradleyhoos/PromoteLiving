e394670b1c503441a03bd30f2bdd62ba0fa4fd2e
malachaifrazier <malachaifrazier@gmail.com>
2015-08-27 22:32:26 -0400

    simple_forn installed & db migrated
    Schema Information annotated on models
    
    whitespace & style cleanup

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150529004916) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "insurances", force: :cascade do |t|
     t.integer  "user_id"
     t.string   "first_name"
@@ -24,7 +27,7 @@ ActiveRecord::Schema.define(version: 20150529004916) do
     t.datetime "updated_at",   null: false
   end
 
-  add_index "insurances", ["user_id"], name: "index_insurances_on_user_id"
+  add_index "insurances", ["user_id"], name: "index_insurances_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -48,7 +51,7 @@ ActiveRecord::Schema.define(version: 20150529004916) do
     t.datetime "updated_at"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
