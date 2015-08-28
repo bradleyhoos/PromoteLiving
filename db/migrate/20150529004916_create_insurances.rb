5ae7ea641cf49bbdd4944f28916aa0937e6e501f
malachaifrazier <malachaifrazier@gmail.com>
2015-08-27 22:40:21 -0400

    whitespace & style cleanup

--- db/migrate/20150529004916_create_insurances.rb
+++ db/migrate/20150529004916_create_insurances.rb
@@ -1,8 +1,8 @@
 class CreateInsurances < ActiveRecord::Migration
   def change
     create_table :insurances do |t|
-      t.belongs_to :user, index: true 
-	  t.string :first_name
+      t.belongs_to :user, index: true
+      t.string :first_name
       t.string :last_name
       t.string :enrolee_id
       t.string :group_number
