bb339ce34ea010b8108991719248275a2b16acbe
malachaifrazier <malachaifrazier@gmail.com>
2015-08-27 22:51:15 -0400

    updated ApplicationCountroller to protect_from_forgery with: :exception

--- Gemfile
+++ Gemfile
@@ -2,7 +2,7 @@ source 'https://rubygems.org'
 
 ruby '2.2.0'
 
-gem 'rails',                '4.2.1'
+gem 'rails',                '4.2.1'#'4.2.2'
 gem 'sass-rails',           '~> 5.0.3'
 gem 'uglifier',             '~> 2.7.0'
 gem 'coffee-rails',         '4.1.0'
--- app/models/user.rb
+++ app/models/user.rb
@@ -33,8 +33,8 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable,
-     :confirmable, :lockable, :timeoutable
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable,
+         :lockable, :timeoutable
 
  def self.authenticate( password)
   user = User.find_by_name(name)
