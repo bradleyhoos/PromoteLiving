9d6ad35d4dbf81d3259f4a14ea37c6872beb2b69
malachaifrazier <malachaifrazier@gmail.com>
2015-08-27 22:34:05 -0400

    Schema Information annotated on models

--- app/models/insurance.rb
+++ app/models/insurance.rb
@@ -1,3 +1,22 @@
+# == Schema Information
+#
+# Table name: insurances
+#
+#  id           :integer          not null, primary key
+#  user_id      :integer
+#  first_name   :string
+#  last_name    :string
+#  enrolee_id   :string
+#  group_number :string
+#  insurer_id   :integer
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+# Indexes
+#
+#  index_insurances_on_user_id  (user_id)
+#
+
 class Insurance < ActiveRecord::Base
   belongs_to :User
   validates :first_name, presence: true, length: {maximum: 50, minimum: 2}
--- test/models/insurance_test.rb
+++ test/models/insurance_test.rb
@@ -1,3 +1,22 @@
+# == Schema Information
+#
+# Table name: insurances
+#
+#  id           :integer          not null, primary key
+#  user_id      :integer
+#  first_name   :string
+#  last_name    :string
+#  enrolee_id   :string
+#  group_number :string
+#  insurer_id   :integer
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+# Indexes
+#
+#  index_insurances_on_user_id  (user_id)
+#
+
 require 'test_helper'
 
 class InsuranceTest < ActiveSupport::TestCase
